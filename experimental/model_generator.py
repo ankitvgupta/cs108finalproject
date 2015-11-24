aa8ef0786437067480fc41d628ef22951797632e
Gabriel Grand <ggrand@college.harvard.edu>
2015-11-24 15:21:07 -0500

    Started working on generator

---
+++ experimental/model_generator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from collections import defaultdict
+import csv
+# Given a string, get's K-mers of length K
+#" dsf asfd adf asdf adsf asdf adsf adsf asdf"
+# https://wiki.python.org/moin/Generators
+
+# returns a generator for the KMers from a string
+def getKMers(K, string):
+    
+    #tokenize string
+    lst = string.split()
+    
+    #iterate through list and grab KMers
+    for x in range (len(lst) - K + 1):
+        yield lst[x:x+K]
+
+def getAllKMers(K, list_of_strings):
+    for string in list_of_strings:
+        #tokenize string
+        lst = string.split()
+
+        #iterate through list and grab KMers
+        for x in range (len(lst) - K + 1):
+            yield lst[x:x+K]
+            
+
+
+def buildModel(K, list_kmers):
+    model = defaultdict(lambda : defaultdict(int))
+    for kmer in list_kmers:
+        current_words = tuple(kmer[:-1])
+        next_word = kmer[-1]
+        model[current_words][next_word] += 1
+    print model
+    
+
+        
+        
