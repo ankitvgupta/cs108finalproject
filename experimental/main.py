3bb9cfd5215233d82199aafdeb98084258ef0983
ANKIT GUPTA <ankitgupta@college.harvard.edu>
2015-12-09 17:09:41 -0500

    removed need to specify filename

--- experimental/main.py
+++ experimental/main.py
@@ -7,11 +7,11 @@ import model_test
 parser = argparse.ArgumentParser()
 #parser.add_argument('target', type=str, help='Twitter handle of target user (omit @)')
 parser.add_argument('-names', '--names', nargs='+', help="Handles", required=True)
-parser.add_argument('-f', type=str, help='Name of file to store downloaded raw tweet data', required=True)
+#parser.add_argument('-f', type=str, help='Name of file to store downloaded raw tweet data', required=True)
 parser.add_argument('-k', type=int, default=3, help='Size of KMer (default = 3)')
 args = parser.parse_args()
 print args
-
+raw_file = "_".join(args.names) + ".csv"
 print args.names
 
 #Default to target twitter handle if no file name specified
@@ -19,8 +19,8 @@ print args.names
 #	args.f = args.target
 
 targets = args.names 		  #Twitter handle
-raw_file = "%s.csv" % args.f  #Raw data file
-raw_file = args.f
+#raw_file = "%s.csv" % f  #Raw data file
+#raw_file = f
 k = args.k 					  #Size of KMer
 
 #Check if data file for target already exists
